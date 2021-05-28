08a834b8aa2d9c06b23223c18cc935bb78e6ba42
Emerson Delatorre de Andrade (VIX Matriz) <emerson@vix.com.br>
2021-05-28 19:24:47 -0300

    Criados componentes Eventos e Palestranstes. Conexão com API para exibição dos dados de Eventos. Adicionado Bootstrap

--- ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,24 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [
-    {
-      tema: 'Angular',
-      local: 'BH'
-    },
-    {
-      tema: '.NET',
-      local: 'RJ'
-    },
-    {
-      tema: 'Vue',
-      local: 'SP'
-    }
-  ]
+  public eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get('https://localhost:5001/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+
+    this.eventos;
   }
 
 }
